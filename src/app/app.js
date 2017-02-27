5bb670f09a9eca6845b36f7c9c314a8d5157e0de
王金宝 <wangjinbao@4zlink.com>
2017-02-27 17:34:51 +0800

    测试接口，开页面

--- src/app/app.js
+++ src/app/app.js
@@ -15,12 +15,25 @@ angular.module('swalk', ['ngRoute','app.home','app.login'])
                 controller:'appRegister',
                 templateUrl:'../app/views/login/register.tpl.html'
             })
-            .otherwise({redirectTo:'/home'});
+            .otherwise({redirectTo:'/login'});
     })
 
     /*所有控制器的父控制器*/
     .controller('rootTabCtrl', ['$scope',function($scope) {
+        /*$(document).ready(function(e) {
+            var counter = 0;
+            if (window.history && window.history.pushState) {
+                $(window).on('popstate', function () {
+                    window.history.pushState('forward', null, '#');
+                    window.history.forward(1);
+                    console.log('88888888888888888')
+                    //alert("不可回退");
+                });
+            }
 
+            window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
+            window.history.forward(1);
+        });*/
     }])
 
     //隐藏底部导航栏
--- src/app/views/home/home.js
+++ src/app/views/home/home.js
@@ -1,4 +1,10 @@
 angular.module('app.home',[])
     .controller('swalkHome',['$scope','$http','$interval',function($scope,$http,$interval){
+        $http({
+            url:'http://172.16.0.92:3080/iyuba',
+            method:'GET'
+        }).success(function(data){
+            console.log(data);
+        })
 
     }])
